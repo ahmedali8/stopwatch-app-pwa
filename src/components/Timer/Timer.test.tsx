b09dfbcca8919b09d73ce92023ccb30ec0558b12
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-11 17:14:51 +0500

    wrote tests and added states and section of timer container in Timer

--- src/components/Timer/Timer.test.tsx
+++ src/components/Timer/Timer.test.tsx
@@ -3,8 +3,20 @@ import { shallow } from 'enzyme';
 import Timer from './Timer';
 
 describe('Timer', () => {
+  let wrapper: any;
+
+  beforeEach(() => (wrapper = shallow(<Timer />)));
+
   it('should display a <div />', () => {
-    const wrapper = shallow(<Timer />);
     expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('should display a `section` with 3 `p` tags', () => {
+    expect(wrapper.find('section').length).toBe(1);
+    expect(wrapper.find('p').length).toBe(3);
+
+    expect(wrapper.find('.hours').text()).toBe('0');
+    expect(wrapper.find('.minutes').text()).toBe('0');
+    expect(wrapper.find('.seconds').text()).toBe('0');
   });
 });
--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Timer() {
-  return <div>Timer</div>;
+  const [timeInSeconds, setTimeInSeconds] = useState<number>(0);
+
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
+
+  const calculateTime: (time: number) => void = (time: number) => {
+    let hours: number, minutes: number, seconds: number;
+
+    hours = Math.floor(time / 3600);
+    minutes = Math.floor((time - hours * 3600) / 60);
+    seconds = Math.floor(time - hours * 3600 - minutes * 60);
+
+    setHours(hours < 10 ? +`0${hours}` : +hours);
+    setMinutes(minutes < 10 ? +`0${minutes}` : +minutes);
+    setSeconds(seconds < 10 ? +`0${seconds}` : +seconds);
+  };
+
+  useEffect(() => {
+    calculateTime(timeInSeconds);
+  }, [timeInSeconds]);
+
+  return (
+    <div>
+      <section className="timer-container">
+        <p className="hours">{hours}</p>
+        <p className="minutes">{minutes}</p>
+        <p className="seconds">{seconds}</p>
+      </section>
+    </div>
+  );
 }
 
 export default Timer;
